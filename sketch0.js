37af94c33652a38ede38d88058cdcede391d596f
Michele La Rosa <miki.larosa@hotmail.it>
2020-02-05 16:28:00 +0100

    Hover Buttons

--- sketch0.js
+++ sketch0.js
@@ -18,37 +18,43 @@ function setup() {
     textFont(myFont);
     textSize(20);
     textAlign(CENTER);
-    text("by Group 12: \n Emanuele Ghebaur, Beatrice Foresti, Pietro Forino, Michele La Rosa", windowWidth/2, windowHeight/10*4.4);
+    text("by Group 12: \n Emanuele Ghebaur, Beatrice Foresti, Pietro Forino, Michele La Rosa", windowWidth / 2, windowHeight / 10 * 4.4);
 
-    button = createButton("START SCRIBBLING");
-    button.position(windowWidth / 2-100, windowHeight/10*5.5);
-    button.size(200, 50);
-    button.mousePressed(openScribble);
-    button.style('background-color', "black");
-    button.style("color", "#ffb3ff");
-    button.style("border-color", "#9fdfff");
-    button.style("font-size", "15px");
-    button.style("font-family", "CircularStd-Book");
+    button1 = createButton("START SCRIBBLING");
+    button1.position(windowWidth / 2 - 100, windowHeight / 10 * 5.5);
+    button1.size(200, 50);
+    button1.mousePressed(openScribble);
+    button1.style('background-color', "black");
+    button1.style("color", "#ffb3ff");
+    button1.style("border-color", "#9fdfff");
+    button1.style("font-size", "15px");
+    button1.style("font-family", "CircularStd-Book");
+    button1.mouseOver(changeColor1);
+    button1.mouseOut(beginningColor1);
 
-    button = createButton("Learn more");
-    button.position(windowWidth / 2-100, windowHeight/10*6.2);
-    button.size(200, 50);
-    //button.mousePressed(openScribble); <-- QUA METTERE PAGINA ABOUT
-    button.style('background-color', "black");
-    button.style("color", "#ffb3ff");
-    button.style("border-color", "#9fdfff");
-    button.style("font-size", "15px");
-    button.style("font-family", "CircularStd-Book");
+    button2 = createButton("Learn more");
+    button2.position(windowWidth / 2 - 100, windowHeight / 10 * 6.2);
+    button2.size(200, 50);
+    //button2.mousePressed(openScribble); <-- QUA METTERE PAGINA ABOUT
+    button2.style('background-color', "black");
+    button2.style("color", "#ffb3ff");
+    button2.style("border-color", "#9fdfff");
+    button2.style("font-size", "15px");
+    button2.style("font-family", "CircularStd-Book");
+    button2.mouseOver(changeColor2);
+    button2.mouseOut(beginningColor2);
 
-    button = createButton("Creative Coding 2019/2020");
-    button.position(windowWidth / 2-100, windowHeight/10*6.9);
-    button.size(200, 50);
-    button.mousePressed(openCourse);
-    button.style('background-color', "black");
-    button.style("color", "#ffb3ff");
-    button.style("border-color", "#9fdfff");
-    button.style("font-size", "15px");
-    button.style("font-family", "CircularStd-Book");
+    button3 = createButton("Creative Coding 2019/2020");
+    button3.position(windowWidth / 2 - 100, windowHeight / 10 * 6.9);
+    button3.size(200, 50);
+    button3.mousePressed(openCourse);
+    button3.style('background-color', "black");
+    button3.style("color", "#ffb3ff");
+    button3.style("border-color", "#9fdfff");
+    button3.style("font-size", "15px");
+    button3.style("font-family", "CircularStd-Book");
+    button3.mouseOver(changeColor3);
+    button3.mouseOut(beginningColor3);
 
 }
 
@@ -57,13 +63,34 @@ function draw() {
 }
 
 function openScribble() {
-
     window.open("scribble.html", "_self");
-
 }
 
+
 function openCourse() {
-
     window.open("https://drawwithcode.github.io/2019/");
+}
 
+function changeColor1() {
+    button1.style('background-color', "#3f3f3f");
+}
+
+function beginningColor1() {
+    button1.style('background-color', "black");
+}
+
+function changeColor2() {
+    button2.style('background-color', "#3f3f3f");
+}
+
+function beginningColor2() {
+    button2.style('background-color', "black");
+}
+
+function changeColor3() {
+    button3.style('background-color', "#3f3f3f");
+}
+
+function beginningColor3() {
+    button3.style('background-color', "black");
 }
