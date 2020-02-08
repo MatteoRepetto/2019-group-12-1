c24d2d1ce671e1249a296ec6db6a2f23c32f59ae
pietro <pietrof@live.com>
2020-02-08 14:03:47 +0100

    logoHome

--- scribble.php
+++ scribble.php
@@ -5,7 +5,6 @@
   <title></title>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
