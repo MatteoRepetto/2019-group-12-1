f7ff394c221cfae26fb5fd98c56e5c184c07fe79
beaforesti <bea_fiore@live.it>
2020-02-10 20:53:18 +0100

    Update scribble_desktop.php

--- scribble_desktop.php
+++ scribble_desktop.php
@@ -21,13 +21,11 @@ body {
 </style>
 </head>
 <body>
-  <img src="assets/logo.png" align="middle"/>
-  <h1 style="color:white; text-align: center"></h1>
-  <h1 style="position: absolute;  left: 10%; top: 48%;">Try to merge the previous scribble in your own drawing!</h1>
-  <h1 style="position: absolute;  right: 10%; top: 48%;">When you're done, press the Loop button in the bottom-right corner.</h1>
-  <h1 style="position: absolute; bottom: 2%;color:white; left: 43%;"></h1>
+  <img src="assets/logo.png" id="mylogo"/>
 
-
+  <h1 style="position: absolute;  left: 5%; top: 48%;">Try to merge the previous scribble <br> in your own drawing! </br></h1>
+  <h1 style="position: absolute;  right: 5%; top: 48%;">When you're done, press <br> the Loop button in the bottom-right corner. </br></h1>
+  <!-- <h1 style="position: absolute; bottom: 2%;color:white; left: 43%;"></h1> -->
 
   <div class="content">
     <iframe src="scribble.php" style="width:100%;border:none;height:100%" />
