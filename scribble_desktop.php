65e09e149e37eb40127de6e1eb8aeec22ae1c570
beaforesti <bea_fiore@live.it>
2020-02-10 21:09:33 +0100

    Update scribble_desktop.php

--- scribble_desktop.php
+++ scribble_desktop.php
@@ -5,28 +5,29 @@
 <link rel="stylesheet" href="/style.css">
 <style>
 
-body {
-  background-color: black;
-}
+  body {
+    background-color: black;
+  }
 
-/* The screen (or content) of the device */
-.content {
-  position: absolute;
-  width: 360px;
-  height: 640px;
-  background: white;
-  left:50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
+  /* The screen (or content) of the device */
+  .content {
+    position: absolute;
+    width: 360px;
+    height: 640px;
+    background: white;
+    left:50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
 </head>
+
 <body>
   <img src="assets/logo.png" id="mylogo"/>
 
-  <h1 style="position: absolute;  left: 5%; top: 30%;">Try to merge the previous scribble <br> in your own drawing! </br></h1>
-  <h1 style="position: absolute;  right: 5%; bottom: 20%;">When you're done, <br> press the Loop button </br> <br> in the bottom-right corner. </br></h1>
-  <!-- <h1 style="position: absolute; bottom: 2%;color:white; left: 43%;"></h1> -->
+  <h1 style="position: absolute;  left: 10%; top: 20%;">Try to merge the previous scribble <br> in your own drawing! </br></h1>
+  <h1 style="position: absolute;  right: 10%; bottom: 20%;">When you're done, <br> press the Loop button <br> in the bottom-right corner. </br></h1>
+
 
   <div class="content">
     <iframe src="scribble.php" style="width:100%;border:none;height:100%" />
