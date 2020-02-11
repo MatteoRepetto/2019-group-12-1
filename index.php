3da64cf0ef12d2231169cb92e2a080deb2dbb6ee
Michele La Rosa <miki.larosa@hotmail.it>
2020-02-11 22:00:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <script language="javascript" type="text/javascript" src="addons/p5.min.js"></script>
     <script language="javascript" type="text/javascript" src="addons/p5.dom.min.js"></script>
     <script language="javascript" type="text/javascript" src="addons/p5.sound.min.js"></script>
+    <link rel="shortcut icon" href="assets/loop.png">
+
     <script src="index.js"></script>
   </head>
   <body>
