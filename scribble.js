cea331c47423b1c5cf71087876a8d4b10bdba9d7
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2020-02-11 12:58:00 +0100

    Update scribble.js

--- scribble.js
+++ scribble.js
@@ -29,8 +29,6 @@ function setup() {
   canvas.mouseReleased(endPath);
   console.log(phpImage);
   //image(img1, 0, 0, 1080 / 5, 1920 / 5)
-  coloreUno = get(fx * 4 + 2, fy * 4 + 1); // pick up the background color
-  coloreDue = get(fx * 4, fy * 4); // pick up the stroke color
   //print("ScoloreUno: " + coloreUno, "ScoloreDue: " + coloreDue);
 }
 
