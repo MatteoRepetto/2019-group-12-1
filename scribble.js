5fcecc0a149a31b6575bdb2fd6f199ff2e28f612
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2020-02-12 00:02:00 +0100

    Restore 23:45

--- scribble.js
+++ scribble.js
@@ -75,10 +75,8 @@ function draw() {
 
   image(img1, 0, 0, 1080 / 5, 1920 / 5)
 
-  coloreUno = get(fx * 4 + 2, fy * 4 + 2); // pick up the background color
-  coloreDue = get(fx * 4+1, fy * 4+2); // pick up the stroke color
-  // coloreUno = get(fx * 4 + 2, fy * 4 + 1); // pick up the background color
-  // coloreDue = get(fx * 4, fy * 4); // pick up the stroke color
+  coloreUno = get(fx * 4 + 2, fy * 4 + 1); // pick up the background color
+  coloreDue = get(fx * 4, fy * 4); // pick up the stroke color
 
   background(coloreDue);
   print("coloreUno: " + coloreUno, "coloreDue: " + coloreDue);
@@ -92,9 +90,6 @@ function draw() {
   rect(0, 0, 2, 2)
   fill(coloreDue)
   rect(3, 0, 2, 2)
-  //rect(1, 1, 3, 3)
-  //fill(coloreDue)
-  //rect(4, 1, 6, 3)
   pop()
 
   pop()
